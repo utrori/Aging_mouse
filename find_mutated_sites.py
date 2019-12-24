c9cb4efec8be3cc198524f2566e4bab8ca03e2fb
Yutaro <yhori@iam.u-tokyo.ac.jp>
2019-12-24 10:39:06 +0900

    Error rate plot

--- find_mutated_sites.py
+++ find_mutated_sites.py
@@ -1,5 +1,7 @@
 import collections
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import glob
 
 len_28S = 5491
@@ -14,17 +16,65 @@ end_58S = 3289
 
 len_ATP = 732
 
+gene2len = {'28S': len_28S, '18S+5.8S': len_18S, 'ATP5b': len_ATP}
+
 with open('/mnt/data/old_mouse/rDNA_index/mouse_rDNA_ATP5b2.fasta') as f:
     ref_data = f.readlines()
-flag = 0
 ref_seq_dict = {}
+flag = 0
 for line in ref_data:
-    if flag == 1:
-        ref_seq_dict[refname] = line.strip()
+    if flag:
+        ref_seq_dict[refname] = line.strip().upper()
         flag = 0
     elif '>' in line:
         flag = 1
         refname = line[1:].strip()
+
+
+def determine_consensus_sequence(base_summary):
+    """Find consensus sequence of given sample data.
+
+    Args:
+        base_summary (dict): a dict from sample name to sample class
+    Returns:
+        gene2cons: a dictionary that returns consensus sequence from gene name
+    """
+    genes2cons = {}
+    for gene in ref_seq_dict.keys():
+        consensus = ''
+        for pos in range(gene2len[gene]):
+            max_freq_bases = []
+            test = []
+            for sample in base_summary.values():
+                bf = sample.base_freq(gene, pos+1)
+                for base, freq in bf.items():
+                    if freq > 0.50:
+                        max_freq_bases.append(base)
+                        test.append((base, freq))
+            if len(max_freq_bases) == 6:
+                if len(set(max_freq_bases)) == 1:
+                    consensus += max_freq_bases[0]
+                else:
+                    consensus += ref_seq_dict[gene][pos]
+            else:
+                consensus += ref_seq_dict[gene][pos]
+        genes2cons[gene] = consensus
+    return genes2cons
+
+
+def base_repertoire(base_freq_dict):
+    """Return which bases are seen as variation.
+
+    Args:
+        base_count_dict (dict): return of base_count.
+    Returns:
+        base_set: a set (literally) of bases seen at the position.
+    """
+    base_rep = []
+    for base, freq in base_freq_dict.items():
+        if freq > 0.01:
+            base_rep.append(base)
+    return set(base_rep)
 
 
 class mouse_sample():
@@ -83,6 +133,27 @@ class mouse_sample():
             freq[base] = count / count_sum
         return freq
 
+    def variation_analysis(self, samples, ref_dict, gene, pos):
+        most_freq_base = 'N'
+        ref_base = ref_dict[gene][pos-1]
+        bf = self.base_freq(gene, pos)
+        non_ref_ratio = 1 - bf[ref_base]
+        if non_ref_ratio < 0.02:
+            return (non_ref_ratio, 'ref')
+        base_rep = base_repertoire(bf)
+        # base_rep is the repertoire of the sample in question.
+        # base_reps is the reportoire of tht other samples
+        base_reps = []
+        for sample in samples.values():
+            base_reps.append(base_repertoire(sample.base_freq(gene, pos)))
+        if all(i == base_rep for i in base_reps):
+            return (non_ref_ratio, 'common')
+        elif all(i == set(ref_base) for i in base_reps) and \
+        len(base_rep) > 1:
+            return (non_ref_ratio, 'unique')
+        else:
+            return (non_ref_ratio, 'not_unique')
+
 
 def minor_mut_rate(sample, gene, coord1, coord2):
     """Return rate of minor mutations in a area of given mouse sample.
@@ -110,7 +181,7 @@ def minor_mut_rate(sample, gene, coord1, coord2):
     return mut_rate
 
 
-def find_major_mutations(sample, gene, coord1, coord2):
+def find_major_variations(sample, gene, coord1, coord2):
     """Return a list of major mutations in a area of given mouse sample.
 
     Here, minor mutations refer to mutations at positions,
@@ -133,20 +204,66 @@ def find_major_mutations(sample, gene, coord1, coord2):
     return muts
 
 
-def plot_mutations(sample, gene, coord1, coord2):
+def analyze_variations(base_summary, ref_dict):
+    """Analyze if the mutation is shared by others etc.
+
+    Args:
+        base_summary (dict): a dict from sample name to sample class
+        ref_dict(dict): dictionary from gene name to the reference sequence
+    Returns:
+    """
+    for gene in ref_seq_dict.keys():
+        for pos in range(gene2len[gene]):
+            bfs = []
+            for sample in base_summary.values():
+                bfs.append(sample.base_freq(gene, pos+1))
+
+
+def plot_mutations(base_summary, sample_name, gene, ref_dict, coord1, coord2):
     """Plot mutation rate.
 
     Args:
-        sample (class): mouse sample class
+        base_summary (dict): dictionary from sample name to sample class
+        sample_name (str): sample name
         gene (str): gene name
+        ref_dict(dict): dictionary from gene name to the reference sequence
         coord1 (int): coordinate
         coord2 (int): coordinate
     """
-    bfs = []
-    for pos in range(coord1, coord2+1):
-        bfs.append(sample.base_freq(gene, pos))
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    sample = base_summary[sample_name]
+    others = {}
+    for k, v in base_summary.items():
+        if k != sample_name:
+            others[k] = v
+    for pos in range(coord1, coord2 + 1):
+        value, er_type = sample.variation_analysis(others, ref_dict, gene, pos)
+        if er_type == 'ref':
+            continue
+            ax.plot(pos, value, marker='.', color='black')
+        elif er_type == 'common':
+            ax.plot(pos, value, marker='.', color=cm.hsv(0.3))
+        elif er_type == 'not_unique':
+            ax.plot(pos, value, marker='.', color=cm.hsv(0.7))
+        elif er_type == 'unique':
+            ax.plot(pos, value, marker='.', color=cm.hsv(1))
+
+    ax.set_xlim(coord1, coord2)
+    ax.set_ylim(0, 1)
+    ax.set_xlabel('Coordinate')
+    ax.set_ylabel('Error rate')
+    plt.savefig('figs/error_rate_plot_ATP_' + sample_name + '.png', dpi=300)
+    plt.close()
+
 
 if __name__ == '__main__':
+    base_summary = pd.read_pickle('base_summary.pkl')
+    genes2cons = determine_consensus_sequence(base_summary)
+    for sample_name in base_summary.keys():
+        plot_mutations(base_summary, sample_name, 'ATP5b', genes2cons, 1, len_ATP)
+    quit()
     path = '/mnt/data/old_mouse/*_counted.txt'
     files = glob.glob(path)
     base_summary = {}
@@ -158,7 +275,8 @@ if __name__ == '__main__':
             countdata = f.readlines()
             base_summary[samplename] = mouse_sample(countdata)
 
+    pd.to_pickle(base_summary, 'base_summary.pkl')
     for sample in samplenames:
         print(sample)
-        print(len(find_major_mutations(base_summary[sample], '28S', sta_28S, end_28S)))
+        print(len(find_major_variations(base_summary[sample], '28S', sta_28S, end_28S)))
         plot_mutations(base_summary[sample], '28S', sta_28S, end_28S)
